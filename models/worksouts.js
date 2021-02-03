190202918a28c57c8c082ac89b059afb4b10d2ae
Kasey Cushing <kasey.cushing@gmail.com>
2021-02-03 17:15:56 -0500

    add provided files, set up server

---
+++ models/worksouts.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  type: {
+    type: String,
+    required: "resistance or cardio required",
+  },
+  name: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: "exercise name required",
+  },
+  duration: {
+    type: Number,
+    min: 1,
+  },
+  distance: {
+    type: Number,
+    min: 1,
+  },
+  weight: {
+    type: Number,
+    min: 1,
+  },
+  reps: {
+    type: Number,
+    min: 1,
+  },
+  sets: {
+    type: Number,
+    min: 1,
+  },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,27 @@
+//set up express
+//make sure all heroku requirements are met
+//set up schema
+const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(logger("dev"));
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//require("./routes/routes")(app)
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//app.listen(PORT, () => {
+// console.log(`App running on port ${PORT}!`);
+//});
