4f9fc2ae5f90133412c0699eb12c83a905192206
Kasey Cushing <kasey.cushing@gmail.com>
2021-02-05 16:21:02 -0500

    html routes for stats and exercise

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,14 @@
 const path = require("path");
 const db = require("../models/workouts");
 
-module.exports = function (app) {};
+module.exports = function (app) {
+  //add exercise
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  //stats
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
